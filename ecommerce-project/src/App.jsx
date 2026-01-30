f07c0ba32545c6fc24409bbb955aa4ca21f0be3b
muhammadomoakin <qasimmuhammadomoakin@gmail.com>
2026-01-30 12:12:35 +0100

    update links to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router";
+import Checkoutpage from "./pages/Checkoutpage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="checkout" element={<div>Checkout page</div>} />
+      <Route path="checkout" element={<Checkoutpage />} />
     </Routes>
   );
 }
